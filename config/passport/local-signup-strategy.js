2c26d0564c965a1da0cb51b8d383b3a6020f33c4
Sule Stone <sulestone@att.net>
2015-10-08 12:48:27 -0400

    added email validation

--- config/passport/local-signup-strategy.js
+++ config/passport/local-signup-strategy.js
@@ -2,13 +2,13 @@ var localStrategy = require('passport-local').Strategy;
 var User = require('../../models/user');
 
 var strategy = new localStrategy(
-{
-  usernameField: 'username',
-  passwordField: 'password',
-  passReqToCallback: true
-},
-function(req, username, password, callback) {
-  User.findOne({'local.username' : username.toLowerCase()}, function(err, user) {
+  {
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  function(req, username, password, callback) {
+    User.findOne({'local.username' : username.toLowerCase()}, function(err, user) {
       console.log('this is user:' + user);
       if(err) return callback(err);
       if(user) {
@@ -18,30 +18,31 @@ function(req, username, password, callback) {
         return callback(null, false, req.flash('error', 'The password must be at least 8 characters long.'));
       }
       //Only one unique email can exist in the database
-      // if(User.findOne({'email' : req.body.email.toLowerCase()})) {
-      //   console.log(req.body.email.toLowerCase());
-      //   console.log('this x: ' + User.findOne({'email' : 'sulestone@att.net'}));
-      //   return callback(null, false, req.flash('error', 'This email is already registered.'));
-      // }
-      if (password !== req.body.confirmPassword) {
-        return callback(null, false, req.flash('error', 'Password does not match.'));
-      }
-      if (!req.body.optionsRadios) {
-        return callback(null, false, req.flash('error', 'Please select an account type.'));
-      }
-      else {
-        var newUser = new User();
-        // Save all user name in downcase
-        newUser.local.username = username.toLowerCase();
-        newUser.local.password = newUser.encrypt(password);
-        //Save all email lowercase
-        newUser.email = req.body.email.toLowerCase();
-        newUser.account = req.body.optionsRadios;
+      User.findOne({'email' : req.body.email.toLowerCase()}, function(err, user) {
+        if(err) return callback(err);
+        if(user) {
+          return callback(null, false, req.flash('error', 'This email is already registered.'));
+        }
+        if (password !== req.body.confirmPassword) {
+          return callback(null, false, req.flash('error', 'Password does not match.'));
+        }
+        if (!req.body.optionsRadios) {
+          return callback(null, false, req.flash('error', 'Please select an account type.'));
+        }
+        else {
+          var newUser = new User();
+          // Save all user name in downcase
+          newUser.local.username = username.toLowerCase();
+          newUser.local.password = newUser.encrypt(password);
+          //Save all email lowercase
+          newUser.email = req.body.email.toLowerCase();
+          newUser.account = req.body.optionsRadios;
 
-        newUser.save(function(err){
-          return callback(err, newUser);
-        });
-      }
+          newUser.save(function(err){
+            return callback(err, newUser);
+          });
+        }
+      });
     });
   }
 );
