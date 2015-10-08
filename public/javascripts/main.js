6d57e0fb1d62b84805b48a6df1f6fd8403684692
Britney Dossett <britneydossett@gmail.com>
2015-10-08 11:45:29 -0400

    Fixed markers and added info windows

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,51 +3,33 @@ function success(position) {
   var map;
   var mapcanvas = document.createElement('div');
   mapcanvas.id = 'mapcontainer';
-  mapcanvas.style.height = '400px';
-  mapcanvas.style.width = '600px';
+  mapcanvas.style.height = '550px';
+  mapcanvas.style.width = '850px';
 
   document.querySelector('article').appendChild(mapcanvas);
 
   var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-// var locations = [
-//      [3.180967,101.715546],
-//      [3.200848,101.616669],
-//      [3.147372,101.597443],
-//      [3.19125,101.710052]
-// ];
+  var mapStyle = [{"featureType":"administrative","stylers":[{"visibility":"on"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#abbaa4"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]},{"featureType":"road.highway","stylers":[{"color":"#ad9b8d"}]}];
+
+  var styledMap = new google.maps.StyledMapType(mapStyle,
+    {name: "Styled Map"});
 
   var options = {
-    zoom: 13,
+    zoom: 14,
     center: coords,
     mapTypeControl: false,
     navigationControlOptions: {
       style: google.maps.NavigationControlStyle.SMALL
     },
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    mapTypeControlOptions: {
+      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
   };
   var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
 
-  // var marker = new google.maps.Marker({
-  //     position: coords,
-  //     map: map,
-  //     title:"You are here!"
-  // });
-// var marker, i;
-
-// for (i = 0; i < locations.length; i++) {
-//     marker = new google.maps.Marker({
-//          position: new google.maps.LatLng(locations[i][0], locations[i][1]),
-//          map: map
-//     });
-
-//     // google.maps.event.addListener(marker, 'click', (function(marker, i) {
-//     //      return function() {
-//     //          infowindow.setContent(locations[i][0]);
-//     //          infowindow.open(map, marker);
-//     //      }
-//     // })(marker, i));
-// }
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
 
 // fetch Places JSON from /data/places
 // loop through and populate the map with markers
@@ -64,46 +46,36 @@ function success(position) {
             console.log("Place = " + place);
             codeAddress(place);
           })
-
-          // loop through places and add markers
-          // for (p in places) {
-          //   //create gmap latlng obj
-          //   tmpLatLng = new google.maps.LatLng( places[p].geo[0], places[p].geo[1]);
-          //   // make and place map maker.
-          //   var marker = new google.maps.Marker({
-          //       map: map,
-          //       position: tmpLatLng,
-          //       title : places[p].name + "<br>" + places[p].geo_name
-          //   });
-          //   bindInfoWindow(marker, map, infowindow, '<b>'+places[p].name + "</b><br>" + places[p].geo_name);
-          //   // not currently used but good to keep track of markers
-          //   markers.push(marker);
-          // }
-        }
+            // bindInfoWindow(marker, map, infowindow, '<b>'+ places.name + "</b><br>" + places.geo_name);
+            // // not currently used but good to keep track of markers
+            // markers.push(marker);
+         }
       })
     }
 fetchPlaces();
 
 
+
   function codeAddress(address) {
-      // var address = "675 Ponce de Leon Ave SE, Atlanta, GA"
-      // console.log(fetchPlaces());
       geocoder.geocode( { 'address': address}, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
-          // map.setCenter(results[0].geometry.location);
           var marker = new google.maps.Marker({
               map: map,
               position: results[0].geometry.location,
               title: "You made it!!!!!!!!!"
           });
-          console.log(marker);
+          var infowindow = new google.maps.InfoWindow({
+          content: 'Address: ' + address
+          });
+          marker.addListener('click', function() {
+          infowindow.open(map, marker);
+          });
         } else {
           alert("Geocode was not successful for the following reason: " + status);
         }
       });
-
     }
-  };
+};
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(success);
